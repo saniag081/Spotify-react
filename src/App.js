ec8e241da25ed5a8c519d744a74e4428f333b29c
saniag081 <sgonzalezhernandez081@gmail.com>
2021-05-15 22:19:47 -0500

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import AppContext from './context/AppContext'
 import useInitialState from './Hooks/useInitialState'
-import Search from './components/Search/index'
+import Home from './Pages/Home'
+import SongDetail from './Pages/SongDetail'
 
 function App() {
   const initialState = useInitialState()
@@ -9,7 +15,12 @@ function App() {
   return (
     <AppContext.Provider value={initialState}>
       <div className="App">
-        <Search />
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/detail' component={SongDetail} />
+          </Switch>
+        </Router>
       </div>
     </AppContext.Provider>
   );
